5157dcb237b37427c22a1fcf20de505f967a621d
ikazirouj <ikazirouj@gmail.com>
2023-07-12 02:48:24 +0900

    comment

--- clock.py
+++ clock.py
@@ -2,15 +2,24 @@
 from tkinter import *
 from time import strftime
 
+##define the main window
 root = Tk()
-root.title("clock")
+##define the name of main window
+root.title("時計だよ")
+#specify the window size
+root.geometry("750x150")
+
+root.configure(bg="white")
 
 def time():
-    clock = strftime("%H:%M:%S %p")
+    ##Conver data structure to "str" and assign to "clock"
+    clock = strftime("%p %H:%M:%S")
+    ##I don't konw! 
     label.config(text=clock)
+    ##start function every 1000 ms
     label.after(1000, time)
 
-label = Label(root, font=("Impact", 80, "bold"), bg="white", fg="black")
+label = Label(root, font=("gTerminal", 100), bg="white", fg="black")
 label.pack(anchor="center")
 time()
 
