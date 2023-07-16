06052a0d424b2e254bdf3c935f8f062c0e043ca7
ikazirouj <ikazirouj@gmail.com>
2023-07-17 06:16:38 +0900

    gg

--- clock.py
+++ clock.py
@@ -12,26 +12,31 @@ root.title("時計だよ")
 root.geometry("850x150")
 #change the main window coloer
 root.configure(bg="white")
-
-
-def time():
-    ##Conver data structure to "str" and assign to "clock"
-    clock = strftime("%p %H:%M:%S")
-    ##I don't konw! 
-    label.config(text=clock)
-    ##start function every 1000 ms
-    label.after(1000, time)
-
-label = Label(root, font=("gTerminal", 100), bg="white", fg="black")
-label.pack(side="left")
-
-canvas = tk.Canvas(root, bg="white", height=200, width=200, highlightthickness=0)
-canvas.pack(side="left")
 chacha = Image.open("chacha.png")
 chacha=chacha.resize((150, 150))
 chacha = ImageTk.PhotoImage(chacha)
 
-canvas.create_image(80, 80, image=chacha)
-time()
+class flont:
+    def clock():
+        global label
+        label = Label(root, font=("gTerminal", 100), bg="white", fg="black")
+        label.pack(side="left")
+    
+    def time():
+        ##Conver data structure to "str" and assign to "clock"
+        clock = strftime("%p %H:%M:%S")
+        ##I don't konw! 
+        label.config(text=clock)
+        ##start function every 1000 ms
+        label.after(1000, flont.time)
+
+    def icon():
+        canvas = tk.Canvas(root, bg="white", height=200, width=200, highlightthickness=0)
+        canvas.pack(side="left")    
+        canvas.create_image(80, 80, image=chacha)
+
+flont.clock()
+flont.time()
+flont.icon()
 
 root.mainloop()
